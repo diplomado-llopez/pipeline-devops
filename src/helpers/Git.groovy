dcc933d055d20880036aeadd2180403906323dd7
ngamboa <ngamboa@insacom.cl>
2022-01-31 21:52:46 -0300

    update git

--- src/helpers/Git.groovy
+++ src/helpers/Git.groovy
@@ -8,7 +8,7 @@ def merge(String ramaOrigen, String ramaDestino) {
 
     checkout(ramaDestino)
 
-    withCredentials([usernamePassword(credentialsId: 'jenkins-gh-ganvoa', gitToolName: 'git-tool')]) {
+    withCredentials([usernamePassword(credentialsId: 'jenkins-gh-ganvoa', gitToolName: 'Default')]) {
         sh '''
             git merge ${ramaOrigen}
             git push origin ${ramaDestino}
