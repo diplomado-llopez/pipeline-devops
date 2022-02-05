f8d49bf4bdeaef5eb97bca02c3ee86bc3c11c58b
targetplaces <luchoz.dd@gmail.com>
2022-02-04 22:14:07 -0300

    issue 5

--- src/helpers/Git.groovy
+++ src/helpers/Git.groovy
@@ -21,7 +21,11 @@ def tag(String rama) {
 }
 
 def checkout(String rama) {
-    sh "git reset --hard HEAD; git checkout -b ${rama}; git pull origin ${rama}"
+    sh "git reset --hard HEAD; git checkout ${rama}; git pull origin ${rama}"
+}
+
+def release(String rama) {
+    sh "git reset --hard HEAD; git checkout -b ${rama}; git pull origin develop"
 }
 
 return this
