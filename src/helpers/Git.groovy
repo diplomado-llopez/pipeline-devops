f47160daacbeeb09b34ca7637ee68d2c389e362c
targetplaces <luchoz.dd@gmail.com>
2022-02-05 20:37:45 -0300

    issue 5

--- src/helpers/Git.groovy
+++ src/helpers/Git.groovy
@@ -25,7 +25,7 @@ def checkout(String rama) {
 }
 
 def release(String rama) {
-    withCredentials([gitUsernamePassword(credentialsId: 'girhub',
+    withCredentials([gitUsernamePassword(credentialsId: 'github',
                  gitToolName: 'default')]){
                          sh "git reset --hard HEAD; git checkout -b ${rama}; git push origin ${rama}"
                  }
