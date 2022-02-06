0d43fa41bb3fcbbec92de1d87f5e03926fc22e13
targetplaces <luchoz.dd@gmail.com>
2022-02-05 22:42:16 -0300

    issue 7

--- src/helpers/Git.groovy
+++ src/helpers/Git.groovy
@@ -17,7 +17,7 @@ def merge(String ramaOrigen, String ramaDestino) {
 }
 
 def tag(String version) {
-    sh "git remote update; git fetch --all; git branch --a; git checkout --track origin/main; git tag ${version}"
+    sh "git remote update; git fetch --all; git branch -a; git checkout --track origin/main; git tag ${version}"
 }
 
 def checkout(String rama) {
